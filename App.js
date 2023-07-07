4eb465678c9c8ffb91b945b87aaf437e712c69b3
shobhit saini <shobhitsaini709@gmail.com>
2023-07-07 12:00:27 +0530

    handle redirect from url to app complete

--- App.js
+++ App.js
@@ -1,4 +1,6 @@
 import 'react-native-gesture-handler';
+import {useEffect, useState} from 'react';
+import {StatusBar, Linking} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 
@@ -7,10 +9,56 @@ import DemoClassScreen from './src/screens/demo-class.screen';
 const Stack = createStackNavigator();
 
 function App() {
+  const [loading, setLoading] = useState(true);
+  const [queryDataFromUrl, setQueryDataFromUrl] = useState(null);
+
+  useEffect(() => {
+    StatusBar.setBarStyle('light-content');
+    StatusBar.setBackgroundColor('#3CCF4E');
+  }, []);
+
+  useEffect(() => {
+    const handleRedirectUrl = url => {
+      if (!url) {
+        console.log('Not from url.');
+        setLoading(false);
+        return;
+      }
+
+      const appPackage = 'com.younglabs';
+      if (url.includes('openStore=true')) {
+        Linking.openURL(`market://details?id=${appPackage}`);
+      }
+
+      let urlParams = {};
+
+      const parseUrl = url.split('?')[1];
+      parseUrl.split('&').forEach(queryData => {
+        const params = queryData.split('=');
+        const queryName = params[0];
+        const queryValue = params[1];
+        urlParams[queryName] = queryValue;
+      });
+
+      setQueryDataFromUrl(urlParams);
+      setLoading(false);
+    };
+
+    Linking.getInitialURL()
+      .then(handleRedirectUrl)
+      .catch(err => console.log(err));
+  }, []);
+
+  if (loading) return null;
+
   return (
     <NavigationContainer>
       <Stack.Navigator screenOptions={{headerShown: false}}>
-        <Stack.Screen name="DemoClass" component={DemoClassScreen} />
+        <Stack.Screen
+          name="DemoClass"
+          component={DemoClassScreen}
+          initialParams={{data: queryDataFromUrl}}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
--- src/screens/demo-class.screen.jsx
+++ src/screens/demo-class.screen.jsx
@@ -1,16 +1,31 @@
-import React from 'react';
 import {StyleSheet, Text, View} from 'react-native';
 import Header from '../components/header.components';
 
-const DemoClassScreen = () => {
+const DemoClassScreen = ({route}) => {
+  const {
+    params: {data},
+  } = route;
+
   return (
     <View>
       <Header />
-      <Text>DemoClassScreen</Text>
+      <Text style={styles.heading}>Welcome to Young Labs</Text>
+      {data ? (
+        <Text>{`Meeting Id = ${data.meetingId} \n Meeting Password = ${data.meetingPassword}`}</Text>
+      ) : (
+        <Text>Comes from without url</Text>
+      )}
     </View>
   );
 };
 
 export default DemoClassScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  heading: {
+    fontSize: 24,
+    fontWeight: '600',
+    color: '#000',
+    textAlign: 'center',
+  },
+});
