9977c6a42203670e0682c5783779c663f4d68f5e
HimanshuKumar79 <ngukumar123@gmail.com>
2024-02-10 18:28:52 +0530

    some ui changes

--- src/components/UserCoursePageComponent/CourseCard.jsx
+++ src/components/UserCoursePageComponent/CourseCard.jsx
@@ -2,17 +2,31 @@ import React, {useEffect, useState} from 'react';
 import {ImageBackground, Pressable} from 'react-native';
 import {Image, Text, View} from 'react-native-animatable';
 import ServiceRequestCard from './ServiceRequestCard';
+import {useSelector} from 'react-redux';
 
 const CourseCard = ({course, navigation}) => {
+  const {
+    bgColor,
+    bgSecondaryColor,
+    textColors,
+    colorYlMain,
+    darkMode,
+    addMoreCourseCardbgColor,
+  } = useSelector(state => state.appTheme);
   return (
     <View
       key={course?.level}
-      className="h-fit w-[96vw] rounded-xl shadow-xl bg-gray-300 mt-[6px] px-2 py-5">
+      style={{backgroundColor: bgSecondaryColor}}
+      className="h-fit w-[96vw] rounded-xl shadow-xl mt-[6px] px-2 py-5">
       <View className="w-full h-fit flex flex-row justify-between items-center mb-2">
-        <Text className="text-[16px] text-black font-semibold">
+        <Text
+          style={{color: textColors?.textPrimary}}
+          className="text-[16px] font-semibold">
           {course?.courseId}
         </Text>
-        <Text className="text-[16px] text-black font-semibold">
+        <Text
+          style={{color: textColors?.textPrimary}}
+          className="text-[16px] font-semibold">
           Student : {course?.childName}
         </Text>
       </View>
