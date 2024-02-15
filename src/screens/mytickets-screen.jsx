b2a2539e651f73ba5a6d6bf3e498f4dea8cac2c1
shobhitsaini <shobhitsaini709@gmail.com>
2024-02-15 10:49:58 +0530

    added my tickets screen

--- src/screens/mytickets-screen.jsx
+++ src/screens/mytickets-screen.jsx
@@ -6,50 +6,49 @@ import {useSelector} from 'react-redux';
 import {authSelector} from '../store/auth/selector';
 
 const MyTickets = () => {
-  const [myTickets, ssetMyTickets] = useState(null);
-  const [loading, setLoading] = useState(false);
+  const [myTickets, ssetMyTickets] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   const {user} = useSelector(authSelector);
 
   const fetchCustomerTickets = async () => {
-    setLoading(true);
-    const token = await auth().currentUser.getIdToken();
-    const API_URL = `${BASE_URL}/admin/tickets/getticketscustomer`;
-    const response = await fetch(API_URL, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: 'Bearer ' + token,
-      },
-      body: JSON.stringify({
-        leadId: user?.leadId,
-      }),
-    });
-    if (response.status === 200) {
-      const result = await response.json();
-      ssetMyTickets(result?.tickets);
-    } else {
-      Showtoast({
-        text: 'Something went wrong while fetching tickets',
-        toast,
-        type: 'danger',
+    try {
+      const token = await auth().currentUser.getIdToken();
+      const API_URL = `${BASE_URL}/admin/tickets/getticketscustomer`;
+      const response = await fetch(API_URL, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: 'Bearer ' + token,
+        },
+        body: JSON.stringify({
+          leadId: user?.leadId,
+        }),
       });
-    }
-    setLoading(false);
+      if (response.status === 200) {
+        const result = await response.json();
+        ssetMyTickets(result?.tickets);
+      } else {
+        Showtoast({
+          text: 'Something went wrong while fetching tickets',
+          toast,
+          type: 'danger',
+        });
+      }
+      setLoading(false);
+    } catch (error) {}
   };
   useEffect(() => {
     fetchCustomerTickets();
   }, []);
 
-  console.log('myTickets', myTickets);
-
-  return (
-    <View>
-      <Text>MyTickets</Text>
-    </View>
-  );
+  return <View style={styles.container}></View>;
 };
 
 export default MyTickets;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
